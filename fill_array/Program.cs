a4824d5013293d9b673d76b03a4ec5bf27d47e31
lattecoff <atkbs@gmail.com>
2021-09-04 18:11:27 +0300

    Добавлено домашнее задание Заполнение массив с клавиатуры.

---
+++ fill_array/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fill_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeArray = 0;
+
+            Console.WriteLine("Введите размер массива");
+            if(int.TryParse(Console.ReadLine(), out sizeArray))
+            {
+                int[] userArray= new int[sizeArray];
+                Console.WriteLine("Введите эелемент массива");
+
+                for(int i = 0; i < sizeArray; i++)
+                {
+                    if (int.TryParse(Console.ReadLine(), out userArray[i]))
+                    {
+                        Console.WriteLine("Введите следующий эелемент массива");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Некорректные данные");
+                        i--;
+                    }
+                }
+
+                for (int i = 0; i < sizeArray; i++)
+                {
+                    Console.WriteLine("Элемент " + i + ": " + userArray[i]);
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
