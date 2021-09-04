67bce58e39b38fd15e8f0858cf4ff6d213bcb03e
lattecoff <atkbs@gmail.com>
2021-09-04 17:51:19 +0300

    Добавлено домашнее задание про треугольники.

---
+++ triangle/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            const int height = 10;
+            const int widght = 10;
+            const int numTriangle = 4;
+
+            int devider = widght;
+
+
+
+            for (int lineNo = 0; lineNo <= height; lineNo++, devider--)
+            {
+                for (int space = 0; space < devider; space++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int tag = devider; tag != widght; tag++)
+                {
+                    Console.Write("#");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            devider++;
+            for (int lineNo = 0; lineNo <= height; lineNo++, devider++)
+            {
+                for (int space = 0; space < devider; space++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int tag = devider; tag != widght; tag++)
+                {
+                    Console.Write("#");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
